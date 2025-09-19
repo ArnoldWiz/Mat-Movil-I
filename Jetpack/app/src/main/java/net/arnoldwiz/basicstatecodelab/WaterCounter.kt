9468d3b913f19756f80b9bf2c96c2b30173ac5d8
Arnold Reyes <166636441+ArnoldWiz@users.noreply.github.com>
2025-09-19 15:49:56 -0600

    Jetpack paso 5

--- Jetpack/app/src/main/java/net/arnoldwiz/basicstatecodelab/WaterCounter.kt
+++ Jetpack/app/src/main/java/net/arnoldwiz/basicstatecodelab/WaterCounter.kt
@@ -9,15 +9,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        val count: MutableState<Int> = mutableStateOf(0)
+        var count by remember { mutableStateOf(0) }
 
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
