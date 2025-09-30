e540e1c37b29efe84e542c24aa1c177aabcd2704
Arnold Reyes <166636441+ArnoldWiz@users.noreply.github.com>
2025-09-30 10:45:43 -0600

    Finalizar la navegacion de rutas y agregar boton cancelar

--- Woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -31,11 +31,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -60,13 +66,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
 /**
  * Composable that displays an app bar and a list of dogs.
  */
 @Composable
 fun WoofApp() {
-    Scaffold { it ->
-        LazyColumn(contentPadding = it) {
+    Scaffold ( topBar = {
+        WoofTopAppBar()
+    }) { it ->
+        LazyColumn (contentPadding = it) {
             items(dogs) {
                 DogItem(
                     dog = it,
@@ -90,7 +124,7 @@ fun DogItem(
 ) {
     Card(modifier = modifier) {
         Row(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .padding(dimensionResource(id = R.dimen.padding_small))
         ) {
@@ -114,12 +148,15 @@ fun DogIcon(
     Image(
         modifier = modifier
             .size(dimensionResource(R.dimen.image_size))
-            .padding(dimensionResource(R.dimen.padding_small)),
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small)
+        ,
         painter = painterResource(dogIcon),
+
 
         // Content Description is not needed here - image is decorative, and setting a null content
         // description allows accessibility services to skip this element during navigation.
-
+        contentScale = ContentScale.Crop,
         contentDescription = null
     )
 }
@@ -141,9 +178,12 @@ fun DogInformation(
         Text(
             text = stringResource(dogName),
             modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+            ,
+            style = MaterialTheme.typography.displayMedium
         )
         Text(
             text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.bodyLarge
         )
     }
 }
@@ -155,14 +195,6 @@ fun DogInformation(
 @Composable
 fun WoofPreview() {
     WoofTheme(darkTheme = false) {
-        WoofApp()
-    }
-}
-
-@Preview
-@Composable
-fun WoofDarkThemePreview() {
-    WoofTheme(darkTheme = true) {
         WoofApp()
     }
 }
